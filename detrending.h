ab921fc2d6f98cb1b2177e9c21aa89aa8b7d3a8c
11beesimaduddin <11beesimaduddin@seecs.edu.pk>
2015-03-01 22:27:10 +0500

    Update detrending.h

--- detrending.h
+++ detrending.h
@@ -1,1 +1,21 @@
+#ifndef DETRENDING_H
+#define DETRENDING_H
 
+#include <math.h>
+
+#include "wavelet2d.h"
+#include "errors.h"
+#include "debug.h"
+
+#ifdef _DEBUG_DETRENDING_
+#include <stdio.h>
+#endif
+
+#define LEVELS 4
+#define WAVELET "db8"
+
+error_t detrending(double **detrended, double *sp_averaged, int nElements);
+
+void normalize(double *signal, int nElements);
+
+#endif
