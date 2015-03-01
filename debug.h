f8e783a7972359fa2cf64e5149caf69bfe8cc303
11beesimaduddin <11beesimaduddin@seecs.edu.pk>
2015-03-01 22:26:10 +0500

    Update debug.h

--- debug.h
+++ debug.h
@@ -1,1 +1,23 @@
+/* Comment definition of _DEBUG_xxx for producing no debug traces
+   Uncomment definition of _DEBUG_xxx to generate all debug traces
 
+   Usage: For all debug code enclose it as follows:
+		#ifdef _DEBUG_xxx
+			// debug trace code here
+		#endif
+*/
+
+#ifndef DEBUG_H
+#define DEBUG_H
+
+//#define _DEBUG_FILE_READING_
+//#define _DEBUG_FACE_DETECTING_
+//#define _DEBUG_FEATURE_TRACKING_
+//#define _DEBUG_ROW_CONVERSION_
+//#define _DEBUG_VIDEO_TRANSFER_
+//#define _DEBUG_BOX_COORDS_
+//#define _DEBUG_SP_AVERAGING_
+//#define _DEBUG_DETRENDING_
+#define _DEBUG_FILTERING_
+
+#endif
