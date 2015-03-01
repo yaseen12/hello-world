eafbd7ecf27dd0a616c73f39e2d9efe8ec8a3ecf
11beesimaduddin <11beesimaduddin@seecs.edu.pk>
2015-03-01 22:34:53 +0500

    Update global_header.h

--- global_header.h
+++ global_header.h
@@ -1,1 +1,22 @@
+#ifndef GLOBAL_HEADER_H
+#define GLOBAL_HEADER_H
 
+#include <assert.h>
+#include <malloc.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "structs.h"
+#include "file_reading.h"
+#include "errors.h"
+#include "debug.h"
+#include "face_detection_and_tracking.h"
+#include "stream_averaging.h"
+#include "detrending.h"
+#include "color_detection.h"
+
+#define NULL 0
+
+#define CLIP_DURATION 0
+
+#endif
